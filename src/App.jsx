45ea0f9de31a9212478b054d7b75042f31ea16e4
FlavienJovic <flavien.jovic@gmail.com>
2023-03-31 10:11:07 +0200

    ajout de la liste des musees

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,52 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const museumList = [
+    {
+      name:"Musée Saint-Remi",
+      adress:"53 rue Simon",
+      imgSrc:"",
+    },
+    {
+      name:"Musée des Beaux-Arts",
+      adress:"8 rue Chanzy",
+      imgSrc:"",
+
+    },
+    {
+      name:"Musée de la rédition du 7 mai 1945",
+      adress:"12 rue du Président Franklin Roosevelt",
+      imgSrc:"",
+    },
+    {
+      name:"Musée Automobile Reims-Champagne",
+      adress:"84 avenue Georges Clemenceau",
+      imgSrc:"",
+    },
+    {
+      name:"Musée du fort de la Pompelle",
+      adress:"RD 944, route de Chalons-en-Champagne",
+      imgSrc:"",
+    },
+    {
+      name:"Planétarium de Reims",
+      adress:"49 avenue du Général de Gaulle",
+      imgSrc:"",
+    },
+    {
+      name:"Musée-Hôtel Le Vergeur",
+      adress:"36 place du Forum",
+      imgSrc:"",
+    }
+  ]
+    
+  
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
     </div>
   )
 }
